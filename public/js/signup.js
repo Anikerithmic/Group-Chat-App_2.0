ae4094b5aee157c726c99ad0142c69b593ca1f8b
Aniket Kakade <aniketkakde55@gmail.com>
2024-01-24 17:05:01 +0530

    Added error message container

--- public/js/signup.js
+++ public/js/signup.js
@@ -3,7 +3,8 @@ const inputName = document.querySelector('#name');
 const inputEmail = document.querySelector('#email');
 const inputPhone = document.querySelector('#phone');
 const inputPassword = document.querySelector('#password');
-const signupButtton = document.querySelector('#signup-button');
+const signupButton = document.querySelector('#signup-button');
+const errorMessageContainer = document.querySelector('#error-message-container');
 
 signupForm.addEventListener('submit', onSubmit);
 
@@ -19,26 +20,20 @@ async function onSubmit(e) {
 
     try {
         const response = await axios.post("http://localhost:5000/signup", userData);
-        console.log('Sign-Up successful:', userData.data);
+        console.log('Sign-Up successful:', response.data);
+
 
         alert(response.data.message);
         window.location.href = './login';
 
         clearInputs();
+        errorMessageContainer.textContent = '';
 
-    }
-    catch (err) {
+    } catch (err) {
         if (err.response && err.response.data && err.response.data.message) {
             errorMessageContainer.textContent = err.response.data.message;
         } else {
             console.log('Error during sign-up:', err);
         }
     }
-}
-
-function clearInputs() {
-    inputName.value = '';
-    inputEmail.value = '';
-    inputPhone.value = '';
-    inputPassword.value = '';
 }
